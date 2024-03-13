b47a126addbe739c9da36f3bab6f81d48d13bcd1
awais-codes <awais.codes1@gmail.com>
2024-03-13 06:18:09 +0500

    chore: updated icon exports

--- src/ui/icons/index.ts
+++ src/ui/icons/index.ts
@@ -1,14 +1,25 @@
 import { default as CodeBracketIcon } from './CodeBracket'
 import { default as HandWaveOutlinedIcon } from './HandWaveOutlined'
+import { default as AcademicHatOulinedIcon } from './AcademicHatOutlined'
+import { default as ChevronUpOutlinedIcon } from './ChevronUpOutlined'
+import { default as DashboardOutlinedIcon } from './DashboardOutlined'
+import { default as GithubIcon } from './Github'
+import { default as LinkedInIcon } from './LinkedIn'
+import { default as MailSolidIcon } from './MailSolid'
 
 const Icons = {
   CodeBracketIcon,
   HandWaveOutlinedIcon,
+  AcademicHatOulinedIcon,
+  ChevronUpOutlinedIcon,
+  DashboardOutlinedIcon,
+  GithubIcon,
+  MailSolidIcon,
+  LinkedInIcon,
 } as const
 
 type IconName = keyof typeof Icons
-type IconProps = (typeof Icons)[IconName]
 
-export type { IconName, IconProps }
+export type { IconName }
 
 export { CodeBracketIcon, HandWaveOutlinedIcon }
